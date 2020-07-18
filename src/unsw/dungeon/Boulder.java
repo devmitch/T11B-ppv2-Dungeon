9830cdd90f35261381f21f8146e2d42547121ad7
Jonathan Clark <jonathan.clark1@student.unsw.edu.au>
2020-07-18 10:06:32 +1000

    Added functionality for a switch to be activated/deactived when a boulder moves on or off it. (#32)

--- src/unsw/dungeon/Boulder.java
+++ src/unsw/dungeon/Boulder.java
@@ -3,10 +3,24 @@ package unsw.dungeon;
 public class Boulder extends Entity {
 
     private Movement movement;
+    private FloorSwitch fs;
 
     public Boulder(Dungeon dungeon, int x, int y) {
         super(dungeon, x, y, true, true, false);
         this.movement = new Movement(dungeon, this);
+        this.fs = findFloorSwitchOnTile();
+        if (fs != null) {
+            fs.activateSwitch();
+        }
+    }
+
+    private FloorSwitch findFloorSwitchOnTile() {
+        for (Entity entity : dungeon.getEntitiesOnTile(getX(), getY())) {
+            if (entity instanceof FloorSwitch) {
+                return (FloorSwitch) entity;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -18,14 +32,32 @@ public class Boulder extends Entity {
 
     @Override
     public void updateState() {
+        // check if still on floor switch
+        if (fs != null) {
+            if (!dungeon.areEntitiesOnSameTile(this, fs)) {
+                fs.deactivateSwitch();
+                fs = null;
+            }
+        }
+
         //kill enemies
+        
     }
+
     /**
      * 
      * @param D
      */
     public void moveInDirection(Direction D) {
         movement.moveInDirection(D);
+    }
+
+    public void setFloorSwitch(FloorSwitch fs) {
+        if (this.fs != null) {
+            this.fs.deactivateSwitch();
+        }
+        this.fs = fs;
+        this.fs.activateSwitch();
     }
 
 }
