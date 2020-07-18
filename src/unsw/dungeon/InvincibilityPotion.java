21d84deb316d0191b245747f433971887f295133
Jonathan Clark <jonathan.clark1@student.unsw.edu.au>
2020-07-18 19:07:00 +1000

    Added behaviour for invincibility potion. (#23)

--- src/unsw/dungeon/InvincibilityPotion.java
+++ src/unsw/dungeon/InvincibilityPotion.java
@@ -2,8 +2,20 @@ package unsw.dungeon;
 
 public class InvincibilityPotion extends Entity {
 
+    private int numberOfStepsLeft;
+
     public InvincibilityPotion(Dungeon dungeon, int x, int y) {
         super(dungeon, x, y, false, false, true);
+        this.numberOfStepsLeft = 16;
     }
-    
+
+    public void decrementNumberOfSteps() {
+        this.numberOfStepsLeft -= 1;
+        System.out.println(numberOfStepsLeft + " steps left with invincibility.");
+    }
+
+    public boolean isInvincible() {
+        return numberOfStepsLeft > 0;
+    }
+
 }
