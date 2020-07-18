9faea52181a89c40a3c8bcf4ae7d99ebbb549ae2
Mitchell Henry <z5254756@ad.unsw.edu.au>
2020-07-18 21:32:25 +1000

    Fixed GoalTree data structure + dungeon completion (#25) (#4)

--- src/unsw/dungeon/GoalTree.java
+++ src/unsw/dungeon/GoalTree.java
@@ -6,9 +6,7 @@ public class GoalTree {
     private List<Goal> goals;
     private TreeComponent root;
 
-    public void updateGoal(Goal goal) {
-        // go into list of goals and update them
-
-        // check if tree (root) is satisfied
+    public boolean isComplete() {
+        return root.isSatisfied();
     }
 }
