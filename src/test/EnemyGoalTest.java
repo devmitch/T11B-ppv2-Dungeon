7fc7d52c9073480d002c25b7299f7f06e920c657
Mitchell Henry <z5254756@ad.unsw.edu.au>
2020-07-19 21:00:31 +1000

    Completed tests for one enemy - both in the case that the player dies or beats the enemy - all passing (#34)

--- src/test/EnemyGoalTest.java
+++ src/test/EnemyGoalTest.java
@@ -24,6 +24,50 @@ public class EnemyGoalTest {
             player.put("x", 3);
             player.put("y", 0);
             player.put("type", "player");
+            entities.put(player);
+
+            json.put("entities", entities);
+
+            JSONObject goal = new JSONObject();
+            goal.put("goal", "enemies");
+            
+            json.put("goal-condition", goal);
+
+            DungeonMockControllerLoader dungeonLoader = new DungeonMockControllerLoader(json);
+    
+            DungeonMockController controller = dungeonLoader.loadController();
+            return controller;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public DungeonMockController setup2() {
+        try {
+            JSONObject json = new JSONObject();
+            json.put("height", 1);
+            json.put("width", 3);
+
+            JSONArray entities = new JSONArray();
+
+            JSONObject player = new JSONObject();
+            player.put("x", 0);
+            player.put("y", 0);
+            player.put("type", "player");
+            entities.put(player);
+
+            JSONObject enemy = new JSONObject();
+            enemy.put("x", 2);
+            enemy.put("y", 0);
+            enemy.put("type", "enemy");
+            entities.put(enemy);
+
+            JSONObject sword = new JSONObject();
+            sword.put("x", 1);
+            sword.put("y", 0);
+            sword.put("type", "sword");
+            entities.put(sword);
 
             json.put("entities", entities);
 
@@ -48,6 +92,34 @@ public class EnemyGoalTest {
         assertNotEquals(null, controller);
         Dungeon dungeon = controller.dungeon;
         assertTrue(dungeon.completedGoal());
+    }
+
+    @Test
+    public void oneEnemyWin() {
+        DungeonMockController controller = setup2();
+        assertNotEquals(null, controller);
+        Dungeon dungeon = controller.dungeon;
+        // check dungeon isn't completed
+        assertFalse(dungeon.completedGoal());
+        // move right, pick up sword, and kill enemy
+        controller.movePlayer(Direction.RIGHT);
+        // check dungeon is completed
+        assertTrue(dungeon.completedGoal());
+    }
+
+    @Test
+    public void oneEnemyLoss() {
+        DungeonMockController controller = setup2();
+        assertNotEquals(null, controller);
+        Dungeon dungeon = controller.dungeon;
+        // check dungeon isn't completed
+        assertFalse(dungeon.completedGoal());
+        // move left until enemy kills player
+        controller.movePlayer(Direction.LEFT);
+        controller.movePlayer(Direction.LEFT);
+        assertTrue(dungeon.getPlayer() == null);
+        // check dungeon goal isn't complete
+        assertFalse(dungeon.completedGoal());
     }
 
 }
