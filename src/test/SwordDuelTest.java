f9bf43e3cff382c055a097134a7c784c4d9aee9b
Mitchell Henry <z5254756@ad.unsw.edu.au>
2020-07-19 15:59:02 +1000

    Created basic template for SwordDuel test

---
+++ src/test/SwordDuelTest.java
@@ -0,0 +1,64 @@
+package test;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import unsw.dungeon.*;
+
+
+public class SwordDuelTest {
+
+    public DungeonMockController setup() {
+        try {
+            JSONObject json = new JSONObject();
+            json.put("width", 5);
+            json.put("height", 5);
+            JSONArray entities = new JSONArray();
+
+            JSONObject player = new JSONObject();
+            player.put("x", 0);
+            player.put("y", 0);
+            player.put("type", "player");
+            entities.put(player);
+
+            JSONObject switch1 = new JSONObject();
+            switch1.put("x", 3);
+            switch1.put("y", 3);
+            switch1.put("type", "switch");
+            entities.put(switch1);
+
+            json.put("entities", entities);
+
+            JSONObject goal = new JSONObject();
+            goal.put("goal", "boulders");
+            json.put("goal-condition", goal);
+
+            DungeonMockControllerLoader dungeonLoader = new DungeonMockControllerLoader(json);
+
+            DungeonMockController controller = dungeonLoader.loadController();
+            return controller;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    @Test
+    public void test1() {
+        DungeonMockController controller = setup();
+        assertNotEquals(controller, null);
+        Dungeon dungeon = controller.dungeon;
+
+    }
+}
